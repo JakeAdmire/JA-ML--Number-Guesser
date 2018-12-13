38fb3b625485f6d353272bcf2dfe00649cc131b1
Melissa LaChasse <mlachasse@Melissas-MacBook-Pro.local>
2018-12-12 21:10:12 -0700

    sumbit button and clear button function

--- main.js
+++ main.js
@@ -12,150 +12,163 @@ var scoreName1 = document.querySelector('.score-name1');
 var scoreName2 = document.querySelector('.score-name2');
 var guessResult1 = document.querySelector('.guess-result1');
 var guessResult2 = document.querySelector('.guess-result2');
-// var guessMessage1 = document.querySelector('.guess-message1');
-// var guessMessage2 = document.querySelector('.guess-message2');
-// var randomNumber = randomNumberGenerator(1, 100)
+var guessMessage1 = document.querySelector('.guess-message1');
+var guessMessage2 = document.querySelector('.guess-message2');
+var allInputFields = document.querySelectorAll('input');
+var randomNumber = randomNumberGenerator(1, 100)
 
 
-// updateButton.addEventListener('click', updateRange);
-// submitButton.addEventListener('click', functionCaller);
+updateButton.addEventListener('click', updateRange);
+submitButton.addEventListener('click', functionCaller);
 
-// function randomNumberGenerator(min, max) {
+allInputFields.forEach(function(inputElement) {
+    inputElement.addEventListener("keyup", clearDisable);
+});
 
-// 	return Math.ceil(Math.random() * (max - min) + min);
-// }
-// function customNumberGenerator() {
-// 	return Math.floor(Math.random() * maxRange.value) + parseInt(minRange.value);
-// }
-// console.log(randomNumber);
-// function updateRange() {
-// 	rangeStart.innerText = minRange.value;
-// 	rangeEnd.innerText = maxRange.value;
-// 	randomNumber = customNumberGenerator();
-// 	console.log("custom number", randomNumber)
-// }
+function randomNumberGenerator(min, max) {
 
-// function submitGuess() {
-// 	scoreName1.innerText = name1.value;
-// 	scoreName2.innerText = name2.value;
-// 	guessResult1.innerText = guess1.value;
-// 	guessResult2.innerText = guess2.value;
-// }
+	return Math.ceil(Math.random() * (max - min) + min);
+}
+function customNumberGenerator() {
+	return Math.floor(Math.random() * maxRange.value) + parseInt(minRange.value);
+}
+console.log(randomNumber);
+function updateRange() {
+	rangeStart.innerText = minRange.value;
+	rangeEnd.innerText = maxRange.value;
+	randomNumber = customNumberGenerator();
+	console.log("custom number", randomNumber)
+}
 
+function submitGuess() {
+	scoreName1.innerText = name1.value;
+	scoreName2.innerText = name2.value;
+	guessResult1.innerText = guess1.value;
+	guessResult2.innerText = guess2.value;
+}
 
-// function compareGuess() {
-// 	console.log(guess1.value);
-// 	if (guess1.value > parseInt(randomNumber)) {
-// 		guessMessage1.innerText = "Your Guess is Too High";
-// 	} else if (guess1.value < parseInt(randomNumber)) {
-// 		guessMessage1.innerText = "Your Guess is Too Low";
-// 	} else {
-// 		guessMessage1.innerText =
-// 	 "BOOM!!!";
-// 	}
-// }
+	
+function compareGuess() {
+	console.log(guess1.value);
+	if (guess1.value > parseInt(randomNumber)) {
+		guessMessage1.innerText = "Your Guess is Too High";
+	} else if (guess1.value < parseInt(randomNumber)) {
+		guessMessage1.innerText = "Your Guess is Too Low";
+	} else {
+		guessMessage1.innerText =
+	 "BOOM!!!";
+	}
+}
 
-// function compareGuess2() {
-// 	console.log(guess2.value);
-// 	if (guess2.value > parseInt(randomNumber)) {
-// 		guessMessage2.innerText = "Your Guess is Too High";
-// 	} else if (guess2.value < parseInt(randomNumber)) {
-// 		guessMessage2.innerText = "Your Guess is Too Low";
-// 	} else {
-// 		guessMessage2.innerText =
-// 	 "BOOM!!!";
-// 	}
-// }
+function compareGuess2() {
+	console.log(guess2.value);
+	if (guess2.value > parseInt(randomNumber)) {
+		guessMessage2.innerText = "Your Guess is Too High";
+	} else if (guess2.value < parseInt(randomNumber)) {
+		guessMessage2.innerText = "Your Guess is Too Low";
+	} else {
+		guessMessage2.innerText =
+	 "BOOM!!!";
+	}
+}
 
-// function functionCaller(event) {
-// 	event.preventDefault();
-// 	submitGuess();
-// 	compareGuess();
-// 	compareGuess2();
-// }
+function functionCaller(event) {
+	event.preventDefault();
+	submitGuess();
+	compareGuess();
+	compareGuess2();
+}
 
+function clearDisable(event) {
+	var enableResetButton = document.querySelector('.reset-game');
+	var enableClearButton = document.querySelector('.clear-game');
+	if (event.target.value != '') {
+    enableResetButton.disabled = false;
+    enableClearButton.disabled = false;
+	}
+}
 
 // When .update-button is pressed 
-updateButton.addEventListener('click', updateRanges);
+// updateButton.addEventListener('click', updateRanges);
 
-function updateRanges(){
-	updateMinRange();
-	updateMaxRange();
-}
+// function updateRanges(){
+// 	updateMinRange();
+// 	updateMaxRange();
+// }
 //   if .min-range-input = ""
 //     range-start = 1
 //   else .range-start = .min-range-input
-function updateMinRange(){
-	if (minRange.value === ''){
-		rangeStart.innerText = 1;
-	} else {
-		rangeStart.innerText = minRange.value;
-	}
-}
+// function updateMinRange(){
+// 	if (minRange.value === ''){
+// 		rangeStart.innerText = 1;
+// 	} else {
+// 		rangeStart.innerText = minRange.value;
+// 	}
+// }
 //   if .max-range-input = ""
 //     range-end = 100
-function updateMaxRange(){
-	if (maxRange.value === ''){
-		rangeEnd.innerText = 100;
-	} else {
-		rangeEnd.innerText = maxRange.value;
-	}
-}
-// When .submit-guess-button is pressed
-submitButton.addEventListener('click', functionCaller);
+// function updateMaxRange(){
+// 	if (maxRange.value === ''){
+// 		rangeEnd.innerText = 100;
+// 	} else {
+// 		rangeEnd.innerText = maxRange.value;
+// 	}
+// }
+// // When .submit-guess-button is pressed
+// submitButton.addEventListener('click', functionCaller);
 
-function functionCaller(event){
-	event.preventDefault();
-	updateFirstName();
-	updateFirstGuess();
-	updateSecondName();
-	updateSecondGuess();
-}
-//   if .name1 = ""
-//     give name1 and name1-error new id that makes name1-error visible and makes name1 have a red border
-//   else
-//     .score-name1 = .name1
-function updateFirstName(){
-	if (name1.value === ''){
-		document.getElementById("name1").classList.add("error");
-	} else {
-		scoreName1.innerText = name1.value;
-		document.getElementById("name1").classList.remove("error");
-	}
-}
+// function functionCaller(event){
+// 	event.preventDefault();
+// 	updateFirstName();
+// 	updateFirstGuess();
+// 	updateSecondName();
+// 	updateSecondGuess();
+// }
+// //   if .name1 = ""
+// //     give name1 and name1-error new id that makes name1-error visible and makes name1 have a red border
+// //   else
+// //     .score-name1 = .name1
+// function updateFirstName(){
+// 	if (name1.value === ''){
+// 		document.getElementById("name1").classList.add("error");
+// 	} else {
+// 		scoreName1.innerText = name1.value;
+// 		document.getElementById("name1").classList.remove("error");
+// 	}
+// }
 
-function updateFirstGuess(){
-	if (guess1.value === ''){
-		document.getElementById("guess1").classList.add("error");
-	} else {
-		guessResult1.innerText = guess1.value;
-		document.getElementById("guess1").classList.remove("error");
-	}
-}
+// function updateFirstGuess(){
+// 	if (guess1.value === ''){
+// 		document.getElementById("guess1").classList.add("error");
+// 	} else {
+// 		guessResult1.innerText = guess1.value;
+// 		document.getElementById("guess1").classList.remove("error");
+// 	}
+// }
 
-function updateSecondName(){
-	if (name2.value === ''){
-		document.getElementById("name2").classList.add("error");
-	} else {
-		scoreName2.innerText = name2.value;
-		document.getElementById("name2").classList.remove("error");
-	}
-}
+// function updateSecondName(){
+// 	if (name2.value === ''){
+// 		document.getElementById("name2").classList.add("error");
+// 	} else {
+// 		scoreName2.innerText = name2.value;
+// 		document.getElementById("name2").classList.remove("error");
+// 	}
+// }
 
-function updateSecondGuess(){
-	if (guess2.value === ''){
-		document.getElementById("guess2").classList.add("error");
-	} else if (guess2.value < minRange.value){
-		document.getElementById("guess2-too-low").classList.add("error");
-	} else if (guess2.value > maxRange.value){
-		document.getElementById("guess2-too-high").classList.add("error");
-	} else {
-		guessResult2.innerText = guess2.value;
-		document.getElementById("guess2").classList.remove("error");
-		document.getElementById("guess2-too-low").classList.remove("error");
-		document.getElementById("guess2-too-high").classList.remove("error");
-	}
-}
+// function updateSecondGuess(){
+// 	if (guess2.value === ''){
+// 		document.getElementById("guess2").classList.add("error");
+// 	} else if (guess2.value < minRange.value){
+// 		document.getElementById("guess2-too-low").classList.add("error");
+// 	} else if (guess2.value > maxRange.value){
+// 		document.getElementById("guess2-too-high").classList.add("error");
+// 	} else {
+// 		guessResult2.innerText = guess2.value;
+// 		document.getElementById("guess2").classList.remove("error");
+// 		document.getElementById("guess2-too-low").classList.remove("error");
+// 		document.getElementById("guess2-too-high").classList.remove("error");
+// 	}
+
 
 //   if .name2 = ""
 //     give name2 and name2-error new id that makes name2-error visible and makes name2 have a red border
